5d28bd138ce3f6910d7dbf51faa7cfbd679140fb
Halfdan Rump <halfdan-rump@garage.co.jp>
2014-04-25 15:01:30 +0900

    Adds config file

---
+++ app/config.py
@@ -0,0 +1,10 @@
+remote_host = 'http://107.170.251.142/'
+
+original_image_dir = 'app/static/images/original/'
+resized_image_dir = 'app/static/images/resized/'
+
+original_image_size = (2784, 1848)
+scale_mean = 0.1
+scale_std = 0.03
+
+http_debug_level = 0
