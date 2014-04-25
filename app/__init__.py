b89ff0c2936b5461f214e2839af65ef8ce931629
Halfdan Rump <halfdan-rump@garage.co.jp>
2014-04-25 15:04:38 +0900

    Adds helper function

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,12 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+
 from app import views
+from app.config import *
+
+def flaskify(path):
+	return path.replace('app', '')
+
+app.flaskify = flaskify
