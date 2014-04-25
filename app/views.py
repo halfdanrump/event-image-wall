432dedcfeeb59f9b3941871820126e441b7b8727
Halfdan Rump <halfdan-rump@garage.co.jp>
2014-04-25 15:05:55 +0900

    Adds upload handler & scans for resized images in root handler

--- app/views.py
+++ app/views.py
@@ -1,14 +1,27 @@
 from app import app
-from flask import render_template
+from flask import render_template, request
 import os
-image_dir = 'app/static/images/'
+import app.config as conf
+
 
 @app.route('/')
 def index():
-	images = set(os.listdir(image_dir))
+	print conf.resized_image_dir
+	images = set(os.listdir(conf.resized_image_dir))
+	images.remove('.DS_Store')
+	images = set(map(lambda x: app.flaskify(conf.resized_image_dir) + x, images))
 	print images
 	return render_template('wall.html', images = images)
 
-@app.route('/upload', methods = ['GET'])
+@app.route('/new', )
+def new_image(images):
+	print images
+
+@app.route('/upload', methods = ['POST'])
 def upload():
+	# save received image to /static/images
+	# emit event to client telling it to append images
+	print 'IMAGE UPLOAD'
+	data = eval(request.data)
+	print ''.join(data['image_name'].split('/')[-2::])
 	return render_template('wall.html')
