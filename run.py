2885e717b64232818b9dd824262397c4d0678c57
Halfdan Rump <halfdan-rump@garage.co.jp>
2014-04-25 15:06:31 +0900

    Changes back to flask webserver

--- run.py
+++ run.py
@@ -6,6 +6,8 @@ from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 from app import app
 
-http_server = HTTPServer(WSGIContainer(app))
-http_server.listen(5000)
-IOLoop.instance().start()
+if __name__ == "__main__":
+	app.run(debug = True)
+# http_server = HTTPServer(WSGIContainer(app))
+# http_server.listen(5000)
+# IOLoop.instance().start()
