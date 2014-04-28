0f4700bb88c713f80ac39440c163603392c7fbc7
Halfdan Rump <halfdan-rump@garage.co.jp>
2014-04-28 12:00:48 +0900

    Removes tornado imports

--- run.py
+++ run.py
@@ -1,8 +1,4 @@
 from app import app, socketio
-#app.run(debug = True)
-from tornado.wsgi import WSGIContainer
-from tornado.httpserver import HTTPServer
-from tornado.ioloop import IOLoop
 import app.config as conf
 from threading import Thread
 
@@ -18,7 +14,6 @@ def image_monitor():
 		images.remove('.DS_Store')
 		images = set(map(lambda x: app.flaskify(conf.resized_image_dir) + x, images))
 		selected_images = sample(images, min(conf.number_of_pictures_on_wall, len(images)))
-		# with app.app_context():
 		socketio.emit('update displayed images',
 					 {'images':selected_images},
 					  namespace = '/test')
@@ -27,6 +22,3 @@ if __name__ == "__main__":
 	Thread(target = image_monitor).start()
 	# app.run(debug = True)
 	socketio.run(app, host = '127.0.0.1', port = 5000)
-# http_server = HTTPServer(WSGIContainer(app))
-# http_server.listen(5000)
-# IOLoop.instance().start()
